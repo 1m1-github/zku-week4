fd7c22e70a7232f3f466b5b6cf24bfd9f55eb0ef
1m1-github <77983409+1m1-github@users.noreply.github.com>
2022-08-03 16:18:01 -0600

    formik for react form attempt to listen to event

--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -1,10 +1,11 @@
 import detectEthereumProvider from "@metamask/detect-provider"
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
 import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
-import { providers } from "ethers"
+import { Contract, providers, utils } from "ethers"
 import Head from "next/head"
 import React from "react"
 import styles from "../styles/Home.module.css"
+import { Formik } from 'formik';
 
 export default function Home() {
     const [logs, setLogs] = React.useState("Connect your wallet and greet!")
@@ -57,7 +58,30 @@ export default function Home() {
         } else {
             setLogs("Your anonymous greeting is onchain :)")
         }
+
+        
     }
+
+    const listenGreeting = async () =>  {
+        const provider = (await detectEthereumProvider()) as any
+
+        await provider.request({ method: "eth_requestAccounts" })
+
+        const ethersProvider = new providers.Web3Provider(provider)
+        const signer = ethersProvider.getSigner()
+
+        const GREETER_ABI = ["event NewGreeting(bytes32 greeting)"]
+        const contract = new Contract(
+            '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+            GREETER_ABI,
+            signer
+        )
+        contract.on("NewGreeting", (greeting: string) => {
+            console.log('NewGreeting');
+            console.log(utils.parseBytes32String(greeting))
+        })
+    }
+    React.useEffect(() => {listenGreeting()}, [])
 
     return (
         <div className={styles.container}>
@@ -74,9 +98,75 @@ export default function Home() {
 
                 <div className={styles.logs}>{logs}</div>
 
+                <div>
+
+                    <Formik
+                        initialValues={{ day: '', month: '' }}
+                        validate={values => {
+                            const errors = {};
+
+                            const dayInt = parseInt(values.day)
+                            if (isNaN(dayInt) || dayInt < 1 || dayInt > 31) {
+                                errors.day = 'wrong';
+                            }
+
+                            const monthInt = parseInt(values.month)
+                            if (isNaN(monthInt) || monthInt < 1 || monthInt > 12) {
+                                errors.month = 'wrong';
+                            }
+
+                            return errors;
+                        }}
+                        onSubmit={(values, { setSubmitting }) => {
+                            console.log(values)
+                            // setTimeout(() => {
+                            //     alert(JSON.stringify(values, null, 2));
+                            //     setSubmitting(false);
+                            // }, 400);
+                        }}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            isSubmitting,
+                            /* and other goodies */
+                        }) => (
+                            <form onSubmit={handleSubmit}>
+                                {'day'}
+                                <input
+                                    type="number"
+                                    name="day"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.day}
+                                />
+                                {errors.day && touched.day && errors.day}
+                                {'month'}
+                                <input
+                                    type="number"
+                                    name="month"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.month}
+                                />
+                                {errors.month && touched.month && errors.month}
+                                <button type="submit" disabled={isSubmitting}>
+                                    Submit
+                                </button>
+                            </form>
+                        )}
+                    </Formik>
+                </div>
+
                 <div onClick={() => greet()} className={styles.button}>
                     Greet
                 </div>
+
+
             </main>
         </div>
     )
